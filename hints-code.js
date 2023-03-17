64691cff93259b583cfb225f2b640756daad46ea
Loom <Loomie@users.noreply.github.com>
2023-03-18 00:55:33 +0100

    fixed warning "invalid end tag"

--- hints-code.js
+++ hints-code.js
@@ -15,21 +15,21 @@ export default {
             </h1>
             <!-- 1st line of the 3-digit code -->
             <div class="panel-block">
-                <input type="text" id="hint1" :placeholder="team1.keywords[team1.code[0] - 1]"></input>
-                <input type="number" id="guess1" min="1" max="4"></input>
-                <input type="number" id="guessOthers1" min="1" max="4"></input>
+                <input type="text" id="hint1" :placeholder="team1.keywords[team1.code[0] - 1]">
+                <input type="number" id="guess1" min="1" max="4">
+                <input type="number" id="guessOthers1" min="1" max="4">
             </div>
             <!-- 2nd line of the 3-digit code -->
             <div class="panel-block">
-                <input type="text" id="hint2" :placeholder="team1.keywords[team1.code[1] - 1]"></input>
-                <input type="number" id="guess2" min="1" max="4"></input>
-                <input type="number" id="guessOthers2" min="1" max="4"></input>
+                <input type="text" id="hint2" :placeholder="team1.keywords[team1.code[1] - 1]">
+                <input type="number" id="guess2" min="1" max="4">
+                <input type="number" id="guessOthers2" min="1" max="4">
             </div>
             <!-- 3rd line of the 3-digit code -->
             <div class="panel-block">
-                <input type="text" id="hint3" :placeholder="team1.keywords[team1.code[2] - 1]"></input>
-                <input type="number" id="guess3" min="1" max="4"></input>
-                <input type="number" id="guessOthers3" min="1" max="4"></input>
+                <input type="text" id="hint3" :placeholder="team1.keywords[team1.code[2] - 1]">
+                <input type="number" id="guess3" min="1" max="4">
+                <input type="number" id="guessOthers3" min="1" max="4">
             </div>
             <!-- Button to submit -->
             <div class="panel-block">

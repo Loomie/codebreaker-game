978c041d2375055c69b3043b9f7aa52384da063d
Loom <Loomie@users.noreply.github.com>
2023-03-19 21:01:14 +0100

    socket.io getting started

---
+++ server/server.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const app = express();
+const http = require('http');
+const server = http.createServer(app);
+const path = require('path');
+const { Server } = require("socket.io");
+const io = new Server(server);
+
+app.use(express.static('..', { index: 'index.html' }))
+
+io.on('connection', (socket) => {
+    console.log('a user connected');
+    socket.on('disconnect', () => {
+        console.log('user disconnected');
+    });
+});
+
+server.listen(12034, () => {
+    console.log('listening on *:12034');
+});

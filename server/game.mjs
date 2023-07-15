08a60a8e5592f9a00e677b5503d24e9a01f55c93
Loom <Loomie@users.noreply.github.com>
2023-07-15 17:19:59 +0200

    both teams must be ready for the next round

--- server/game.mjs
+++ server/game.mjs
@@ -94,21 +94,28 @@ export function receiveGuess(player, guess) {
     }
 }
 
+// remembers which team has finished which round and wants to proceed to the next round
+const readyRound = { team1: 0, team2: 0 }
 export function nextRound(player) {
     const encoding1 = data.team1.encoding
+    const encoding2 = data.team2.encoding
     if (encoding1.state.phase == GamePhase.Results && data.team1.team.hasPlayer(player)) {
         console.log(`next round from player ${player.playerName} for team1`)
+        readyRound.team1 = data.round
         copyHints(encoding1, data.team1)
-        encoding1.nextPhase()
     } else {
-        const encoding2 = data.team2.encoding
         if (encoding2.state.phase == GamePhase.Results && data.team2.team.hasPlayer(player)) {
             console.log(`next round from player ${player.playerName} for team2`)
+            readyRound.team2 = data.round
             copyHints(encoding2, data.team2)
-            encoding2.nextPhase()
         } else {
             console.log(`next round but wrong phase`)
         }
+    }
+    // check if both teams are ready
+    if (readyRound.team1 === data.round && readyRound.team2 === data.round) {
+        encoding1.nextPhase()
+        encoding2.nextPhase()
     }
 }
 
